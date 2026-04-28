99065b4ea7292664513afbda2c87fb3aeeeca6d3
anisaekaputrii00-star <anisaekaputrii00@gmail.com>
2026-04-28 14:40:20 +0700

    menghapus node dari memory dan menampilkan konfirmasi

--- DoubelLinkedList.cpp
+++ DoubelLinkedList.cpp
@@ -124,6 +124,9 @@ public:
                 current->next->prev = current->prev;
         }
 
-
+         // Step 5: Delete the node
+        delete current;
+        cout << "Record with roll number " << rollNo << " deleted" << endl;
     }
+
 }
