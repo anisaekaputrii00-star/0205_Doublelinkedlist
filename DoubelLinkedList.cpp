69127e6d222eb47cd1caea9b246a709e83d405d5
anisaekaputrii00-star <anisaekaputrii00@gmail.com>
2026-04-28 13:54:24 +0700

    validasi duplikat dan insert di awal

--- DoubelLinkedList.cpp
+++ DoubelLinkedList.cpp
@@ -34,7 +34,29 @@ public:
         //step 2: Assign value to the data fields
         newNode->noMhs = nim;
 
-        
+        //step 3: Insert at begining if List is empty or nim is smallest
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplicate number not allowed" << endl;
+                return;
+            }
+
+            //step 4: newNode.next = START
+            newNode->next = START;
+            
+            //step 5 START.prev = newNode (if START exists)
+            if (START != NULL)
+                START->prev = newNode;
+                
+            // step 6: newNode.prev = NULL    
+            newNode->prev = NULL;
+
+            //step 7: START = newNode
+            START = newNode;
+            return;
+        }
 
         //
     }
