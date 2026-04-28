4f5b0ecb3d5f3a939b57c48adb8b96f1fdb638c0
anisaekaputrii00-star <anisaekaputrii00@gmail.com>
2026-04-28 14:57:16 +0700

    mencari node terakhir

--- DoubelLinkedList.cpp
+++ DoubelLinkedList.cpp
@@ -161,4 +161,13 @@ public:
             cout << "\nList is empty" << endl;
             return;
         }
+
+        // Step 1: Move to Last node
+        Node *currentNode = START;
+        int i = 0;
+        while (currentNode->next != NULL)
+        {
+            currentNode = currentNode->next;
+            i++;
+        }
 }
